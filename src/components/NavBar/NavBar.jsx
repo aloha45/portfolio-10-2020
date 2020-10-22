a8b48cf04478c9aa835480829b0a425698aaab83
Cory Spicer <corys4139@gmail.com>
2020-10-21 21:00:51 -0400

    add in projects

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,16 +4,16 @@ import './NavBar.css'
 const NavBar = () => {
     return ( 
         <React.Fragment>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Cory Spicer</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Cory Spicer</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                <a class="nav-link" href="#">Projects <span class="sr-only">(current)</span></a>
-                <a class="nav-link" href="#">About Me</a>
-                <a class="nav-link" href="#">Contact</a>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-link" href="#">Projects <span class="sr-only">(current)</span></a>
+                    <a className="nav-link" href="#">About Me</a>
+                    <a className="nav-link" href="#">Contact</a>
                 </div>
             </div>
             </nav>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,13 +2,42 @@ import React, { Component } from 'react'
 
 class Projects extends Component {
     state = { 
-
+        projects: [
+            {
+            unit: 1,
+            name: 'Plus One',
+            link: '#',
+            screenshot: ''
+            },
+            {
+            unit: 2,
+            name: 'Survivor Database',
+            link: '#',
+            screenshot: ''
+            },
+            {
+            unit: 3,
+            name: 'Beets',
+            link: '#',
+            screenshot: ''
+            },
+            {
+            unit: 4,
+            name: 'Dapjeongneo',
+            link: '#',
+            screenshot: ''
+            }
+        ]
      }
 
     render() { 
         return ( 
             <React.Fragment>
                 <h2>Cory rules</h2>
+                <h3>Check out his projects:</h3>
+                {this.state.projects.map((project) => (
+                    <div>{project.name}</div>
+                ))}
             </React.Fragment>
          );
     }
